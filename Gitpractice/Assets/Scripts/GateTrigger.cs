f062251d1621ff7e1e82f414023ffeace8d63dbd
jackdenkin1374 <kevincheng137@gmail.com>
2019-09-23 19:21:10 -0400

    testscene scene - basic
    Ground
    Wall
    Player capsule
    AI capsule
    non-working gate with pressure plate trigger - need to work on animations for it and see if that would be better

---
+++ Gitpractice/Assets/Scripts/GateTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTrigger : MonoBehaviour
+{
+    //Need to revise and use animations
+
+    [SerializeField] private Transform gate;
+
+    public float speed;
+    private bool isOpened = false;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    private void Start()
+    {
+        startPos = gate.transform.position;
+        endPos = gate.transform.position + gate.transform.up * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isOpened == false)
+        {
+            Debug.Log("Opening gate");
+            isOpened = true;
+            Vector3 targetPosition = new Vector3(gate.transform.position.x, 30, gate.transform.position.z);
+
+            float timeStartedLerping = 0f;
+            float lerpTime = 1f;
+            float currentLerpTime;
+
+            timeStartedLerping += Time.deltaTime;
+            if (timeStartedLerping > lerpTime)
+            {
+                timeStartedLerping = lerpTime;
+            }
+
+            //float timeSinceStarted = Time.time - timeStartedLerping;
+            float percentageCompleted = timeStartedLerping / lerpTime;
+
+            gate.transform.position = Vector3.Lerp(gate.transform.position, targetPosition, percentageCompleted);
+
+        }
+    }
+}
