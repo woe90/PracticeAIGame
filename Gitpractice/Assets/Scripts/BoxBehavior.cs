697d36ba68f8b5e7b98ce0a82ae7a2f318f0400a
jackdenkin1374 <kevincheng137@gmail.com>
2019-10-01 10:46:58 -0400

    testscene - grab WIP
    Grab functions WIP

---
+++ Gitpractice/Assets/Scripts/BoxBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehavior : MonoBehaviour, IGrabbable
+{
+    FixedJoint joint;
+
+    private void Awake() {
+        joint = GetComponent<FixedJoint>();
+    }
+
+    public void Grab() {
+        joint.autoConfigureConnectedAnchor = true;
+    }
+
+    public void Drop() { 
+        joint.autoConfigureConnectedAnchor = false;
+    }
+}
--- Gitpractice/Assets/Scripts/PlayerController.cs
+++ Gitpractice/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     private float moveSpeed;
     private float turnSpeed;
+
     private Transform thingToPull; // null if nothing, else a link to some pullable crate
 
     private void Start() {
@@ -18,6 +19,11 @@ public class PlayerController : MonoBehaviour
 
     private void Update() {
         move();
+        if (Input.GetKey(KeyCode.E)) {
+            Debug.Log("Grabbing");
+            //AttemptGrab();              //attempt the grab when the key has been pressed
+
+        }
     }
 
     private void move() {
@@ -50,6 +56,72 @@ public class PlayerController : MonoBehaviour
             rb.velocity = Vector3.zero;
         }
     }
+
+    /*private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        // We use gravity and weight to push things down, we use
+        // our velocity and push power to push things other directions
+        if (hit.moveDirection.y < -0.3)
+        {
+            force = new Vector3(0, -0.5f, 0) * movement.gravity * weight;
+        }
+        else
+        {
+            force = hit.controller.velocity * pushPower;
+        }
+
+        // Apply the push
+        body.AddForceAtPosition(force, hit.point);
+
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
+    }*/
+
+    /*private void AttemptGrab()
+    {
+        if (objectToGrab != null)
+        {
+            objectToGrab.Grab();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IGrabbable grabbableObject = other.GetComponent<IGrabbable>();
+        if (grabbableObject != null)
+        {
+            objectToGrab = grabbableObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<IGrabbable>() != null)
+        {
+            objectToGrab = null;
+        }
+    }*/
 
     // For grabing boxes and moving them
     /*private void OnControllerColliderHit(ControllerColliderHit hit)
