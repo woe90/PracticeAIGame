c67e004770f05d081c8a2aa66a8aad4069b76236
gbruss <gbruss@uncc.edu>
2019-10-15 09:13:59 -0400

    camera follow
    added script for camera follow

---
+++ Gitpractice/Assets/AIController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+	[SerializeField] private Transform trans;
+    [SerializeField] private Rigidbody rb;
+
+    private float moveSpeed;
+    private float turnSpeed;
+
+    private Transform thingToPull; // null if nothing, else a link to some pullable crate
+
+    private void Start() {
+        moveSpeed = 4f;
+        turnSpeed = 10f;
+    }
+
+    private void Update() {
+        move();
+        if (Input.GetKey(KeyCode.E)) {
+            Debug.Log("Grabbing");
+            //AttemptGrab();              //attempt the grab when the key has been pressed
+
+        }
+    }
+
+    private void move() {
+        Vector3 inputVector = Vector3.zero;
+
+        // Check for movement input
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector += Vector3.forward;
+        } else if (Input.GetKey(KeyCode.S)) {
+            inputVector += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector += Vector3.left;
+        } else if (Input.GetKey(KeyCode.D)) {
+            inputVector += Vector3.right;
+        }
+
+        if (inputVector != Vector3.zero) {
+
+            // Normalize input vector to standardize movement speed
+            inputVector.Normalize();
+            inputVector *= moveSpeed;
+            rb.velocity = inputVector;
+
+            // Face player along movement vector
+            Quaternion targetRotation = Quaternion.LookRotation(inputVector);
+            trans.rotation = Quaternion.Lerp(trans.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        } else {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Kinematic Arrive HW/Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	    Vector3 targetPosition;
+		Vector3 lookAtTarget;
+		Quaternion playerRot;
+		float rotSpeed = 5;
+		float speed = 5;
+		float radiusOfSat = 2;
+		bool isMoving = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+        	SetTargetPosition();
+        }
+        if(isMoving == true)
+        {
+        	speed = 5;
+        	transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+    		transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    		//targetPosition.Normalize();
+    		if (targetPosition.magnitude < radiusOfSat)
+    			isMoving = false;
+    			speed = 0;
+        }
+    }
+
+    void SetTargetPosition()
+    {
+    	Ray click = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	RaycastHit hit;
+
+    	if (Physics.Raycast(click, out hit, 2000))
+    	{
+    		targetPosition = hit.point;
+    		lookAtTarget = new Vector3(targetPosition.x - transform.position.x, transform.position.y, targetPosition.z - transform.position.z);
+    		playerRot = Quaternion.LookRotation(lookAtTarget);
+    		isMoving = true;
+    	}
+    }
+
+}
