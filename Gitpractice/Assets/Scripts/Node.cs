3cb5750970183f1dbf3c2d2a0556fc238666ab08
jackdenkin1374 <kevincheng137@gmail.com>
2019-11-22 10:58:58 -0500

    Tutorial Scene

--- Gitpractice/Assets/Scripts/Node.cs
+++ Gitpractice/Assets/Scripts/Node.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Node {
+    public bool ground;
     public bool obstacle;
     public Vector3 worldPosition;
     public int XPos;
@@ -13,7 +14,8 @@ public class Node {
     public int hCost;
     public Node parent;
 
-    public Node(bool blocked, Vector3 worldPos, int x, int y) {
+    public Node(bool walkable, bool blocked, Vector3 worldPos, int x, int y) {
+        ground = walkable;
         obstacle = blocked;
         worldPosition = worldPos;
         XPos = x;
--- Gitpractice/Assets/Scripts/WorldDecomposer.cs
+++ Gitpractice/Assets/Scripts/WorldDecomposer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class WorldDecomposer : MonoBehaviour {
     public static Node[,] worldData2;
 
+    private static int[] worldSize;
+    private static int[] startWorld;
+
     private static int nodeSize;
 
     private static int terrainWidth;
@@ -14,6 +17,7 @@ public class WorldDecomposer : MonoBehaviour {
     private static int cols;
 
     private void Start() {
+        SetWorldSizes();
 
         terrainWidth = 40;
         terrainLength = 40;
@@ -49,8 +53,15 @@ public class WorldDecomposer : MonoBehaviour {
 
                 RaycastHit hit;
 
+<<<<<<< Updated upstream
                 // Bit shift the index of the layer (9) to get a bit mask
                 int layerMask = 1 << 9;
+=======
+                // Bit shift the index of the layer (8) to get a bit mask
+                int obstacleMask = 1 << 8;
+                int targetMask = 1 << 9;
+                int groundMask = 1 << 10;
+>>>>>>> Stashed changes
 
                 // This would cast rays only against colliders in layer 9.
 
@@ -59,13 +70,17 @@ public class WorldDecomposer : MonoBehaviour {
                 //layerMask = ~layerMask;
 
                 // Does the ray intersect any objects excluding the player layer
-                if (Physics.Raycast(startPos, Vector3.down, out hit, Mathf.Infinity, layerMask)) {
+                if (Physics.Raycast(startPos, Vector3.down, out hit, Mathf.Infinity, obstacleMask) &&
+                    Physics.Raycast(startPos, Vector3.down, out hit, Mathf.Infinity, groundMask)) {
                     print("Hit something at row: " + row + " col: " + col);
                     Debug.DrawRay(startPos, Vector3.down * 5, Color.red, 50000);
-                    worldData2[row, col] = new Node(true, worldPos, row, col);
-                } else {
+                    worldData2[row, col] = new Node(true, true, worldPos, row, col);
+                } else if (Physics.Raycast(startPos, Vector3.down, out hit, Mathf.Infinity, groundMask)) {
                     Debug.DrawRay(startPos, Vector3.down * 5, Color.green, 50000);
-                    worldData2[row, col] = new Node(false, worldPos, row, col);
+                    worldData2[row, col] = new Node(true, false, worldPos, row, col);
+                } else {
+                    Debug.DrawRay(startPos, Vector3.down * 5, Color.yellow, 50000);
+                    worldData2[row, col] = new Node(false, false, worldPos, row, col);
                 }
             }
         }
@@ -103,5 +118,25 @@ public class WorldDecomposer : MonoBehaviour {
             }
         }
         return neighbours;
+    }
+
+    private void SetWorldSizes() {
+        worldSize[0] = 10;
+        worldSize[1] = 10;
+        worldSize[2] = 10;
+        worldSize[3] = 10;
+        worldSize[4] = 10;
+        worldSize[5] = 10;
+        worldSize[6] = 10;
+        worldSize[7] = 10;
+
+        startWorld[0] = 10;
+        startWorld[1] = 10;
+        startWorld[2] = 10;
+        startWorld[3] = 10;
+        startWorld[4] = 10;
+        startWorld[5] = 10;
+        startWorld[6] = 10;
+        startWorld[7] = 10;
     }
 }
