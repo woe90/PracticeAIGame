5795734425b0b98d055826a1cfb1b83d1cc47c85
brannon <shananeko90@gmail.com>
2019-11-24 17:04:49 -0500

    Simple Pitfall made
    Test with belt, box goes over edge and a space appears above it.

---
+++ Gitpractice/Assets/Scripts/Pitfall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pitfall : MonoBehaviour
+
+
+{
+    [SerializeField] GameObject CrossWalk;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Destroy(collision.gameObject);
+
+        CrossWalk.SetActive(true);
+    }
+}
