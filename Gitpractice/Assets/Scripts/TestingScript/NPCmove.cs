c6161965fc5935528da16c85a7e4c9713819b569
brannon <shananeko90@gmail.com>
2019-11-26 19:30:27 -0500

    2nd part
    Added script to a cube to move to a target, the sphere. Navmesh baked into the 1st ploane

---
+++ Gitpractice/Assets/Scripts/TestingScript/NPCmove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Transform _destination;
+    NavMeshAgent _navMeshAgent;
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    // Update is called once per frame
+    private void SetDestination()
+    {
+
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+
+        }
+
+    }
+}
