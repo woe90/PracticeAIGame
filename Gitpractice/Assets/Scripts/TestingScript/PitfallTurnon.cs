b5a89f245f4f82f3f2316c80279a4eb8b3229f37
brannon <shananeko90@gmail.com>
2019-11-26 19:49:09 -0500

    3rd part, spawn crosswalk with pitfall usage
    belt drops a cube into the pitfall, makes the crosswalk.
    Now need to fix some of the Navmesh and give AI the chance the ability to not move

---
+++ Gitpractice/Assets/Scripts/TestingScript/PitfallTurnon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallTurnon : MonoBehaviour
+
+
+{
+    [SerializeField] GameObject CrossWalk;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Destroy(collision.gameObject);
+        MeshRenderer MeshComponent = gameObject.GetComponent<MeshRenderer>();
+
+        CrossWalk.SetActive(true);
+
+        MeshComponent.enabled = true;
+    }
+}
